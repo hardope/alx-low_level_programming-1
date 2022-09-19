a8b6c23e55062e4427178170c778aea5d1cf2773
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-19 13:32:37 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,23 @@
 #include<stdio.h>
+#include "2-strlen.c"
 
 /**
-* _puts - Prints to stdout
-* @str: string
-* Return: Length of string
-*/
-void _puts(char *str)
+ * puts_half - puts half of int
+ *
+ * @str: string to split
+ */
+void puts_half(char *str)
 {
-	printf("%s\n", str);
+	int i;
+	int check = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		check += 1;
+	}
+	for (i = (_strlen(str) + check) / 2; i < _strlen(str); i++)
+	{
+		printf("%c", str[i]);
+	}
+	printf("\n");
 }
