e17f5b15dfb68a714df4542dfbf0c154a0e091ab
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-19 16:54:41 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,11 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-* _puts - Prints to stdout
-* @str: string
-* Return: Length of string
-*/
-void _puts(char *str)
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-	printf("%s\n", str);
+char a[100];
+	int r, n, i;
+
+	n = 0;
+	i = 0;
+	srand(time(NULL));
+	while (n < 2645)
+	{
+		r = rand() % 122;
+		if (r > 32)
+		{
+			a[i++] = r;
+			n += r;
+		}
+	}
+	a[i++] = (2772 - n);
+	a[i] = '\0';
+	printf("%s", a);
+
+	return (0);
 }
