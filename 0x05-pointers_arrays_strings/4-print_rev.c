958553dfc741d9a50b1a753b803960c23853edf6
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-19 12:45:22 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -21,8 +21,8 @@ void print_rev(char *s)
 	for (i = 0; i < len/2; i++)
 	{
 		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		s[i] = s[len - i];
+		s[len - i] = temp;
 	}
 	printf("%s\n", s);
 }
