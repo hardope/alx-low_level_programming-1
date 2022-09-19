c6152bdc4e190c27d84756b0e329695b2ef9b053
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-19 13:37:42 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,46 @@
 #include<stdio.h>
+#include "2-strlen.c"
 
 /**
-* _puts - Prints to stdout
-* @str: string
-* Return: Length of string
-*/
-void _puts(char *str)
+ * _atoi - converts string to integer
+ * @s: string to convert
+ *
+ * Return: returns integer value
+ */
+int _atoi(char *s)
 {
-	printf("%s\n", str);
+	int i;
+	int np = 0;
+	int c;
+	int d = 1;
+	int num = 0;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
+			break;
+		if (s[i] == '-')
+			np--;
+		if (s[i] == '+')
+			np++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			c++;
+		}
+	}
+	while (c > 0)
+	{
+		num += ((s[i - 1] - '0') * d);
+		i--;
+		c--;
+		d *= 10;
+	}
+	if (np >= 0)
+	{
+		num *= 1;
+	} else
+	{
+		num *= -1;
+	}
+	return (num);
 }
