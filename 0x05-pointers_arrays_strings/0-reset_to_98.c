0f68f0375db1a4e0b3844916bdadaaad15304847
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-19 09:01:03 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+/**
+* reset_to_98 - Reset to 98
+*/
+void reset_to_98(int *n)
+{
+	&n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void reset_to_98(int *n);
+
+#endif
