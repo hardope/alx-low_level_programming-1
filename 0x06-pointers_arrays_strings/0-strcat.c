eaa51f1020d3e9ae85e45c7bc39fedea9a15ff1e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-21 14:41:46 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,18 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	srtcat(*dest, *src);
-	return (*dest);
+	int i;
+	int j;
+
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[j + i] = src[i];
+	}
+	dest[j + i] = '\0';
+
+	return (dest);
+
 }
