8f01fb427a472a6c3f9b9e7fae048412d5fb9b4f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-21 20:33:39 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,13 +1,43 @@
-#include<stdio.h>
 #include "main.h"
-#include <string.h>
 /**
-* _strcat - Main Function
-* @dest: Parameter
-* @src: parameter
-*/
-char *_strcat(char *dest, char *src)
+ * print_number - prints number
+ * @n:integer to convert to character
+ *
+ */
+void print_number(int n)
 {
-	srtcat(*dest, *src);
-	return (*dest);
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n += 1;
+		n *= -1;
+		n++;
+	}
+	abs = n;
+	abSCount = n;
+
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
