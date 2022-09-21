cc4e69a607b981aff1a700b4a98f0702b4decf0e
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-21 11:58:58 +0100

    Add All Files

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+* _strcat - Main Function
+* @dest: Parameter
+* @src: parameter
+*/
+char *_strcat(char *dest, char *src)
+{
+	srtcat(*dest, *src);
+	return (*dest);
+}
