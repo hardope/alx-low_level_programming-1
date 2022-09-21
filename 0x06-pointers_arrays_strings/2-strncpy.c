c273d1a6c0554a5e85e395f94f1844e73a2025d1
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-21 20:02:09 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
-#include "main.h"
-#include <string.h>
 /**
-* _strcat - Main Function
-* @dest: Parameter
-* @src: parameter
-*/
+ * _strncpy - copies a string
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: amount of places to copy
+ * Return: value of dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
