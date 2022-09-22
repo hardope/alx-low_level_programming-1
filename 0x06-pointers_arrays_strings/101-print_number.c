47d114980b5b11cb8cadb5a9ec3cd8254a1f8e96
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-22 12:11:31 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,6 +2,41 @@
 /**
  * print_number - prints number
  * @n:integer to convert to character
- *
  */
 void print_number(int n)
+{
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n += 1;
+		n *= -1;
+		n++;
+	}
+	abs = n;
+	abSCount = n;
+
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
+}
