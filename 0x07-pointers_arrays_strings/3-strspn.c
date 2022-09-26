3cea30fec1ee74aff18b9ef31f1bddd6097c8a0c
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-26 11:42:21 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,8 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
  * @accept: string to check against
- *
  * Return: number of bytes of s in accept
  */
 unsigned int _strspn(char *s, char *accept)
