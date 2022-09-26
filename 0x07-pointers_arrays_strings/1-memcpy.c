e3b63e0d41ba1fcd6045e0811e260bbf3bdfdad8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-26 08:45:37 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,9 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _memcpy - copies memory area
  * @dest: destination of copy
  * @src: memory area to copy
  * @n: number of bytes to copy
- *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
