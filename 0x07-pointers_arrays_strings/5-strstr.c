a7b18ecf7c6a5b0f4164a55508ac2925e8ecbd43
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-26 11:43:50 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,8 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * _strstr - locates a substring
  * @haystack: string in which to check for needle
  * @needle: substring to find in haystack
- *
  * Return: pointer to beginning of needle in haystack or NULL if no match
  */
 char *_strstr(char *haystack, char *needle)
