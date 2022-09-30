a015f1a7c1e441589b405bc14ada917e34a0ee50
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-30 13:36:21 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: pointers to strings of arguments
  * Return: always 0
  */
-int main(char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
