bc200995cce3c05093a15d11cf1e504dad0462f8
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-09-30 07:50:54 +0100

    Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ * @argc: number of arguments passed at command
+ * @argv: pointers to strings of arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
