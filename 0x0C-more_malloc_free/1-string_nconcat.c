819a5a347fb5df1f693bb38230b9bae0077e3b4f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-10-06 07:52:16 +0100

    Editing Files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - concatenates two strings.
  * @s1: first string to copy
  * @s2: second string to copy
  * @n: number of bytes of s2 to copy
- *
  * Return: char pointer to newly allocated place in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
