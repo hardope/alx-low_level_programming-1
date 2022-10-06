8be591dff4bdbdb2c57de13524acb072d3726927
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-10-06 07:52:29 +0100

    Editing Files

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * malloc_checked - allocates memory using malloc
  * @b: memory to allocate
- *
  * Return: pointer to allocated memory or normal process termination if error
  */
 void *malloc_checked(unsigned int b)
