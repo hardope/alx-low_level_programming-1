24223602a36d9846fa1b7a1d7c32a8d29df78435
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-10-10 11:41:12 +0100

    All

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+# 0x0C. C - Preprocessor
+
