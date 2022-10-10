d7036c30209b197402acb403fe7b813813b5afb3
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2022-10-10 11:27:53 +0100

    all

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
